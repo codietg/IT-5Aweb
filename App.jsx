d1556322d67ba99048412d8450538a7492741d5e
tanishka gupta <183907828+codietg@users.noreply.github.com>
2025-11-03 13:07:54 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Book from './components/book.jsx'
+
+function App() {
+  return (
+    <div>
+      <Book />
+    </div>
+  )
+}
+
+export default App
+
---
+++ BOOKAPP.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function BOOKAPP() {
+  return (
+    <div id='book'>
+        <img src=" " alt=""/>
+         <img src = "" alt = ""/>
+        <h1>Title:Math</h1>
+        <h1>250</h1>
+
+    </div>
+  )
+}
+
+export default BOOKAPP
